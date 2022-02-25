fcac0cc9312509dc0dc548f832f4880e1f7b2f29
Fandy Adam <fandy_noville@bca.co.id>
2022-02-25 23:28:00 +0700

    no message

---
+++ src/main/java/com/fandy/circleci/Application.java
@@ -0,0 +1,35 @@
+package com.fandy.youtube;
+
+import com.fandy.circleci.DailyUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan({"com.fandy"})
+public class Application implements CommandLineRunner {
+
+    private DailyUpdate dailyUpdate;
+
+    @Autowired
+    public Application(
+        DailyUpdate dailyUpdate
+    ) {
+        this.dailyUpdate = dailyUpdate;
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).headless(false).run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        dailyUpdate.run();
+    }
+
+
+}
---
+++ src/main/java/com/fandy/circleci/DailyUpdate.java
@@ -0,0 +1,19 @@
+package com.fandy.circleci;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class DailyUpdate {
+
+    public void run() {
+
+    }
+}
